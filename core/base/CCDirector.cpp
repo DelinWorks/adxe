4c430512ed2c6a429339183b4903d8c778412e2d
Turky Mohammed <45469625+DelinWorks@users.noreply.github.com>
2023-04-27 20:29:26 +0300

    Update CCDirector.cpp

--- core/base/CCDirector.cpp
+++ core/base/CCDirector.cpp
@@ -619,7 +619,7 @@ void Director::setProjection(Projection projection)
         Mat4 matrixPerspective, matrixLookup;
 
         // issue #1334
-        Mat4::createPerspective(60, (float)size.width / size.height, 10, zeye + size.height / 2, &matrixPerspective);
+        Mat4::createPerspective(60, (float)size.width / size.height, 0.01, UINT_MAX, &matrixPerspective);
 
         Vec3 eye(size.width / 2, size.height / 2, zeye), center(size.width / 2, size.height / 2, 0.0f),
             up(0.0f, 1.0f, 0.0f);
