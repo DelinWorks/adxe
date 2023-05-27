bce1fb1b533080d3b7fcafa1a84c7874f9f2c90c
Turky Mohammed <45469625+DelinWorks@users.noreply.github.com>
2023-05-27 16:04:39 +0300

    Update CCParticleSystemQuad.cpp

--- core/2d/CCParticleSystemQuad.cpp
+++ core/2d/CCParticleSystemQuad.cpp
@@ -471,11 +471,7 @@ void ParticleSystemQuad::updateParticleQuads()
                 for (int i = 0; i < _particleCount;
                      ++i, ++quad, ++r, ++g, ++b, ++a, ++hue, ++sat, ++val, ++fadeDt, ++fadeLn)
                 {
-                    float colorR = *r;
-                    float colorG = *g;
-                    float colorB = *b;
-                    float colorA = *a * (*fadeDt / *fadeLn) * (getOpacity() / 255.0);
-                    hsv.set(colorR, colorG, colorB, colorA);
+                    hsv.fromRgba({*r, *g, *b, *a * (*fadeDt / *fadeLn)});
                     hsv.h += *hue;
                     hsv.s     = abs(*sat);
                     hsv.v     = abs(*val);
@@ -496,11 +492,7 @@ void ParticleSystemQuad::updateParticleQuads()
                 for (int i = 0; i < _particleCount;
                      ++i, ++quad, ++r, ++g, ++b, ++a, ++hue, ++sat, ++val, ++fadeDt, ++fadeLn)
                 {
-                    float colorR = *r;
-                    float colorG = *g;
-                    float colorB = *b;
-                    float colorA = *a * (*fadeDt / *fadeLn) * (getOpacity() / 255.0);
-                    hsv.set(colorR, colorG, colorB, colorA);
+                    hsv.fromRgba({*r, *g, *b, *a * (*fadeDt / *fadeLn)});
                     hsv.h += *hue;
                     hsv.s    = abs(*sat);
                     hsv.v    = abs(*val);
@@ -522,7 +514,7 @@ void ParticleSystemQuad::updateParticleQuads()
                     uint8_t colorR = *r * *a * 255;
                     uint8_t colorG = *g * *a * 255;
                     uint8_t colorB = *b * *a * 255;
-                    uint8_t colorA = *a * (*fadeDt / *fadeLn) * (getOpacity() / 255.0) * 255;
+                    uint8_t colorA = *a * (*fadeDt / *fadeLn) * 255;
                     quad->bl.colors.set(colorR, colorG, colorB, colorA);
                     quad->br.colors.set(colorR, colorG, colorB, colorA);
                     quad->tl.colors.set(colorR, colorG, colorB, colorA);
@@ -536,7 +528,7 @@ void ParticleSystemQuad::updateParticleQuads()
                     uint8_t colorR = *r * 255;
                     uint8_t colorG = *g * 255;
                     uint8_t colorB = *b * 255;
-                    uint8_t colorA = *a * (*fadeDt / *fadeLn) * (getOpacity() / 255.0) * 255;
+                    uint8_t colorA = *a * (*fadeDt / *fadeLn) * 255;
                     quad->bl.colors.set(colorR, colorG, colorB, colorA);
                     quad->br.colors.set(colorR, colorG, colorB, colorA);
                     quad->tl.colors.set(colorR, colorG, colorB, colorA);
@@ -559,11 +551,7 @@ void ParticleSystemQuad::updateParticleQuads()
                 auto hsv = HSV();
                 for (int i = 0; i < _particleCount; ++i, ++quad, ++r, ++g, ++b, ++a, ++hue, ++sat, ++val)
                 {
-                    float colorR = *r;
-                    float colorG = *g;
-                    float colorB = *b;
-                    float colorA = *a;
-                    hsv.set(colorR, colorG, colorB, colorA);
+                    hsv.fromRgba({*r, *g, *b, *a});
                     hsv.h += *hue;
                     hsv.s     = abs(*sat);
                     hsv.v     = abs(*val);
@@ -583,11 +571,7 @@ void ParticleSystemQuad::updateParticleQuads()
                 auto hsv = HSV();
                 for (int i = 0; i < _particleCount; ++i, ++quad, ++r, ++g, ++b, ++a, ++hue, ++sat, ++val)
                 {
-                    float colorR = *r;
-                    float colorG = *g;
-                    float colorB = *b;
-                    float colorA = *a;
-                    hsv.set(colorR, colorG, colorB, colorA);
+                    hsv.fromRgba({*r, *g, *b, *a});
                     hsv.h += *hue;
                     hsv.s    = abs(*sat);
                     hsv.v    = abs(*val);
